1a8c756da85dbb77ee4dddb3461f8936de653c07
Khaintg Soe htet <khaingsoehtet11@gmail.com>
2025-04-24 14:48:41 +0630

    Refactor database connection to use environment variables and update Docker Compose to include .env file

--- db.php
+++ db.php
@@ -1,9 +1,9 @@
 <?php
-$host = 'morningkids-db'; // Docker service name
-$db   = 'morningkids';
-$user = 'user';
-$pass = 'password';
-$charset = 'utf8mb4';
+$host = getenv('DB_HOST');
+$db   = getenv('DB_NAME');
+$user = getenv('DB_USER');
+$pass = getenv('DB_PASS');
+$charset = getenv('DB_CHARSET') ?: 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 $options = [
